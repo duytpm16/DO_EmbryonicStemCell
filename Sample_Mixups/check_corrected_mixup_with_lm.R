32a2452016335a5098a130740c561716fc96b287
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-13 12:36:41 -0500

    Update check_corrected_mixup_with_lm.R

--- Sample_Mixups/check_corrected_mixup_with_lm.R
+++ Sample_Mixups/check_corrected_mixup_with_lm.R
@@ -6,60 +6,60 @@ lm_QTL <-
     threshold <- as.vector(threshold)
     
     
-    ### Perform for each threshold
+    ### For each threshold...
     for(i in threshold){
       
-      high_peaks <- lod.peaks %>%
-        filter(lod >= i & cis == TRUE)
+      
+        # Subset QTLs above threshold i
+        high_peaks <- lod.peaks %>%
+                                filter(lod >= i & cis == TRUE)
       
       
       
-      # Creating matrix to store predicted values
-      predicted_matrix           <- as.data.frame(matrix(0, nrow = length(dimnames(genoprobs[[1]])[[1]]), ncol = nrow(high_peaks)))
-      rownames(predicted_matrix) <- dimnames(genoprobs[[1]])[[1]]
-      column_names               <- character()
+        # Creating matrix to store predicted values
+        predicted_matrix           <- as.data.frame(matrix(0, nrow = length(dimnames(genoprobs[[1]])[[1]]), ncol = nrow(high_peaks)))
+        rownames(predicted_matrix) <- dimnames(genoprobs[[1]])[[1]]
+        column_names               <- character()
       
       
       
       
       
-      ### Run regression on each pQTL
-      for(j in 1:nrow(high_peaks)){
+        ### For each QTL
+        for(j in 1:nrow(high_peaks)){
         
-        # Extract pQTL info
-        expr_column <- high_peaks$annot.id[j]
-        qtl_chr     <- high_peaks$qtl.chr[j]
-        qtl_marker  <- high_peaks$marker.id[j]
-        
-        
-        
-        
-        
-        # Get expression of one of the pQTL
-        y <- expr[ ,expr_column]
+            # Extract QTL info
+            expr_column <- high_peaks$annot.id[j]
+            qtl_chr     <- high_peaks$qtl.chr[j]
+            qtl_marker  <- high_peaks$marker.id[j]
         
         
         
         
         
+            # Get expression of one of the pQTL
+            y <- expr[ ,expr_column]
         
-        # Run lm on pQTL if no missing values are present
-        if(all(!is.na(y))){
-          column_names <- c(column_names, expr_column)               # Storing name of expression if it passes the if statement
-          geno         <- genoprobs[[qtl_chr]][,,qtl_marker]         # Get the genotype probability at the pQTL in matrix form
+        
+        
+        
+        
+        
+            # Run lm on pQTL if no missing values are present
+            if(all(!is.na(y))){
+               column_names <- c(column_names, expr_column)               # Storing name of expression if it passes the if statement
+               geno         <- genoprobs[[qtl_chr]][,,qtl_marker]         # Get the genotype probability at the pQTL in matrix form
+              
+               full_matrix  <- data.frame(cbind(pheno = y, geno, covar))  # Combine expression, genotype probability at pQTL, and covariates together for lm function
           
           
+               # lm begins
+               fit <- lm(pheno ~ ., data = full_matrix)                   # Fit lm          
+               predicted_matrix[,j] <- predict(fit)                       # Get predicted value
           
-          full_matrix  <- data.frame(cbind(pheno = y, geno, covar))          # Combine expression, genotype probability at pQTL, and covariates together for lm function
-          
-          
-          # lm begins
-          fit <- lm(pheno ~ ., data = full_matrix)                           # Fit lm          
-          predicted_matrix[,j] <- predict(fit)
-          
-        } # if
+            } # if
         
-      } # for j
+        } # for j
       
       
       
