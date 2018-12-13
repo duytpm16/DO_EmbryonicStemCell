51d50245330fb298a8f5aeec86ebaaf63a5f6666
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-13 11:04:36 -0500

    Update and rename lm_miQTL.R to miQTL_mixup_check.R

--- Sample_Mixups/miQTL_mixup_check.R
+++ Sample_Mixups/miQTL_mixup_check.R
@@ -2,16 +2,23 @@
 #    1.) threshold to subset the lod.peaks data
 lm_QTL <- 
   function(threshold){
-    
+   
+    ### Make threshold parameter a vector to loop over
     threshold <- as.vector(threshold)
     
     
     
+    ### For each threshold...
     for(i in threshold){
       
+      
+      
+        # Subset lod.peaks to contain QTLs above threshold i and make sure they are cis
         high_peaks <- lod.peaks %>%
                                 filter(lod >= i & cis == TRUE)
       
+      
+        
       
       
         # Creating matrix to store predicted values
@@ -22,31 +29,45 @@ lm_QTL <-
       
       
       
-      
+        ### For each QTL...
         for(j in 1:nrow(high_peaks)) {
-            # Extract pQTL info
+            
+            # Extract QTL info
             expr_column <- high_peaks$annot.id[j]
             qtl_chr     <- high_peaks$qtl.chr[j]
             qtl_marker  <- high_peaks$marker.id[j]  
             
             
-            this_y <- expr[,expr_column]
-            this_K <- K[[qtl_chr]]
-            this_X_Q <- genoprobs[[qtl_chr]][,,qtl_marker]
-            this_X_Q_2 <- cbind(rep(1, nrow(this_X_Q)), this_X_Q[,-which.max(colSums(this_X_Q))])
-            this_predict <- probs[[qtl_chr]][,,qtl_marker]
-            this_predict_2 <-  cbind(rep(1, nrow(this_predict)), this_predict[,-which.max(colSums(this_X_Q))])
           
+            # Get required data for model fitting and prediction
+            this_y <- expr[,expr_column]                                                                        # Observed expressions
+            this_K <- K[[qtl_chr]]                                                                              # Kinship matrix as QTL chromosome
+            this_X_Q <- genoprobs[[qtl_chr]][,,qtl_marker]                                                      # Genoprob at QTL chromosome and marker 195 x 8
+            this_X_Q_2 <- cbind(rep(1, nrow(this_X_Q)), this_X_Q[,-which.max(colSums(this_X_Q))])               # Combine intercept to genoprob, used for model fitting
+            this_predict <- probs[[qtl_chr]][,,qtl_marker]                                                      # Get same probabilities in full MUGA array at QTL chromosome and marker 969 x 8
+            this_predict_2 <-  cbind(rep(1, nrow(this_predict)), this_predict[,-which.max(colSums(this_X_Q))])  # Add intercept to probabilities used for prediction
+          
+          
+            # Rename first column of probabilities to Intercept
             colnames(this_X_Q_2)[1] <- "Intercept"
             colnames(this_predict_2)[1] <- "Intercept"
+          
+          
+            # Get covariates
             this_X_covar <- dataset$covar
 
+          
+          
+            # Fit Null model
             if(j == 1) {
                null_fit <- lmmbygls(y = this_y, X = cbind(rep(1, nrow(this_X_covar)), this_X_covar), K = this_K)
             }
           
+            # Fit Alt model
             alt_fit <- lmmbygls(y = this_y, X = cbind(this_X_Q_2, this_X_covar), K = this_K[names(this_y), names(this_y)])
           
+          
+            # Get predicted value 
             predicted_matrix[,j]  <- this_predict_2 %*% alt_fit$coefficients[colnames(this_predict_2)]
      
         }
@@ -60,11 +81,7 @@ lm_QTL <-
       colnames(predicted_matrix) <- high_peaks$annot.id
       observed_matrix <- as.data.frame(expr[,high_peaks$annot.id])
       
-      
-      # stopifnot(rownames(predicted_matrix) == rownames(observed_matrix))
-      # stopifnot(colnames(predicted_matrix) == colnames(observed_matrix))
-      
-      
+       
       
       ### Save predicted and observed matrix to global environment
       assign(paste0('predicted_matrix_',i), predicted_matrix, envir = .GlobalEnv)
