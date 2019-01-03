a91495ce6ca8bd6744db092e0080a42e3f97803d
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-01-03 12:42:44 -0500

    Update function_lm_MUGA_miQTL.R

--- Sample_Mixups/function_lm_MUGA_miQTL.R
+++ Sample_Mixups/function_lm_MUGA_miQTL.R
@@ -2,7 +2,7 @@
 #    1.) threshold to subset the lod.peaks data
 lm_MUGA_miQTL <- 
   function(threshold){
-   
+    
     ### Make threshold parameter a vector to loop over
     threshold <- as.vector(threshold)
     
@@ -13,65 +13,68 @@ lm_MUGA_miQTL <-
       
       
       
-        # Subset lod.peaks to contain QTLs above threshold i and make sure they are cis
-        high_peaks <- lod.peaks %>%
-                                filter(lod >= i & cis == TRUE)
+      # Subset lod.peaks to contain QTLs above threshold i and make sure they are cis
+      high_peaks <- lod.peaks %>%
+        filter(lod >= i & cis == TRUE)
       
       
+      
+      
+      
+      # Creating matrix to store predicted values
+      predicted_matrix           <- as.data.frame(matrix(0, nrow = length(dimnames(probs[[1]])[[1]]), ncol = nrow(high_peaks)))
+      rownames(predicted_matrix) <- dimnames(probs[[1]])[[1]]
+      
+      
+      
+      
+      
+      ### For each QTL...
+      for(j in 1:nrow(high_peaks)) {
         
-      
-      
-        # Creating matrix to store predicted values
-        predicted_matrix           <- as.data.frame(matrix(0, nrow = length(dimnames(probs[[1]])[[1]]), ncol = nrow(high_peaks)))
-        rownames(predicted_matrix) <- dimnames(probs[[1]])[[1]]
+        # Extract QTL info
+        expr_column <- high_peaks$annot.id[j]
+        qtl_chr     <- high_peaks$qtl.chr[j]
+        qtl_marker  <- high_peaks$marker.id[j]  
+        
+        
+        
+        # Get required data for model fitting and prediction
+        this_y <- expr[,expr_column]
+        this_y <- this_y[!is.na(this_y)]                # Observed expressions
+        this_K <- K[[qtl_chr]]
+        this_K <- this_K[names(this_y),names(this_y)]  # Kinship matrix as QTL chromosome
+        this_X_Q <- genoprobs[[qtl_chr]][,,qtl_marker]                                                      # Genoprob at QTL chromosome and marker 195 x 8
+        this_X_Q_2 <- cbind(rep(1, nrow(this_X_Q)), this_X_Q[,-which.max(colSums(this_X_Q))])               # Combine intercept to genoprob, used for model fitting
+        this_X_Q_2 <- this_X_Q_2[names(this_y),]
+        this_predict <- probs[[qtl_chr]][,,qtl_marker]                                                      # Get same probabilities in full MUGA array at QTL chromosome and marker 969 x 8
+        this_predict_2 <-  cbind(rep(1, nrow(this_predict)), this_predict[,-which.max(colSums(this_X_Q))])  # Add intercept to probabilities used for prediction
 
-      
-      
-      
-      
-        ### For each QTL...
-        for(j in 1:nrow(high_peaks)) {
-            
-            # Extract QTL info
-            expr_column <- high_peaks$annot.id[j]
-            qtl_chr     <- high_peaks$qtl.chr[j]
-            qtl_marker  <- high_peaks$marker.id[j]  
-            
-            
-          
-            # Get required data for model fitting and prediction
-            this_y <- expr[,expr_column]                                                                        # Observed expressions
-            this_K <- K[[qtl_chr]]                                                                              # Kinship matrix as QTL chromosome
-            this_X_Q <- genoprobs[[qtl_chr]][,,qtl_marker]                                                      # Genoprob at QTL chromosome and marker 195 x 8
-            this_X_Q_2 <- cbind(rep(1, nrow(this_X_Q)), this_X_Q[,-which.max(colSums(this_X_Q))])               # Combine intercept to genoprob, used for model fitting
-            this_predict <- probs[[qtl_chr]][,,qtl_marker]                                                      # Get same probabilities in full MUGA array at QTL chromosome and marker 969 x 8
-            this_predict_2 <-  cbind(rep(1, nrow(this_predict)), this_predict[,-which.max(colSums(this_X_Q))])  # Add intercept to probabilities used for prediction
-          
-          
-            # Rename first column of probabilities to Intercept
-            colnames(this_X_Q_2)[1] <- "Intercept"
-            colnames(this_predict_2)[1] <- "Intercept"
-          
-          
-            # Get covariates
-            this_X_covar <- dataset$covar
-
-          
-          
-            # Fit Null model
-            if(j == 1) {
-               null_fit <- lmmbygls(y = this_y, X = cbind(rep(1, nrow(this_X_covar)), this_X_covar), K = this_K)
-            }
-          
-            # Fit Alt model
-            alt_fit <- lmmbygls(y = this_y, X = cbind(this_X_Q_2, this_X_covar), K = this_K[names(this_y), names(this_y)])
-          
-          
-            # Get predicted value 
-            predicted_matrix[,j]  <- this_predict_2 %*% alt_fit$coefficients[colnames(this_predict_2)]
-     
+        
+        # Rename first column of probabilities to Intercept
+        colnames(this_X_Q_2)[1] <- "Intercept"
+        colnames(this_predict_2)[1] <- "Intercept"
+        
+        
+        # Get covariates
+        this_X_covar <- covar[names(this_y),]
+        
+        
+        
+        # Fit Null model
+        if(j == 1) {
+          null_fit <- lmmbygls(y = this_y, X = cbind(rep(1, nrow(this_X_covar)), this_X_covar), K = this_K)
         }
         
+        # Fit Alt model
+        alt_fit <- lmmbygls(y = this_y, X = cbind(this_X_Q_2, this_X_covar), K = this_K[names(this_y), names(this_y)], na.action = na.omit)
+        
+        
+        # Get predicted value 
+        predicted_matrix[,j]  <- this_predict_2 %*% alt_fit$coefficients[colnames(this_predict_2)]
+        
+      }
+      
       
       
       
@@ -81,11 +84,11 @@ lm_MUGA_miQTL <-
       colnames(predicted_matrix) <- high_peaks$annot.id
       observed_matrix <- as.data.frame(expr[,high_peaks$annot.id])
       
-       
+      
       
       ### Save predicted and observed matrix to global environment
       assign(paste0('predicted_matrix_',i), predicted_matrix, envir = .GlobalEnv)
       assign(paste0('observed_matrix_',i), observed_matrix, envir = .GlobalEnv)
       
     } # for i in threshold
-}
+ }
