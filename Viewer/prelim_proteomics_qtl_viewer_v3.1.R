a2b2a20e8c52a4b1c15ec64cd8dabaf5bc14db84
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-01-18 12:17:30 -0500

    Update and rename prelim_proteomics_qtl_viewer_v3.R to prelim_proteomics_qtl_viewer_v3.1.R

--- Viewer/prelim_proteomics_qtl_viewer_v3.1.R
+++ Viewer/prelim_proteomics_qtl_viewer_v3.1.R
@@ -30,60 +30,32 @@ orig_samples_key <- readRDS('do_esc_sample_ids.rds')
 
 
 
+### Get correct ID labels for samples.
+#   Lines 35-55 is taken from Selcan (Munger Lab).
+matches <- matches %>%
+                   filter(!is.na(sampleid.esc_prot)) %>%
+                   select(-ends_with(".npc_rna"), -ends_with(".esc_rna")) %>%
+                   distinct() %>%
+                   arrange(PBID)
+colnames(matches) <- gsub("\\.esc_prot","",colnames(matches))
+duplicates        <- matches[duplicated(matches$top_muga),]
 
+mixed   <- matches %>%
+                   filter(mixup ==T & !endsWith(matches$sampleid,"_rep2") ) %>%
+                   mutate(correct_id = ifelse(PBID %in% duplicates$PBID, paste0(PBID,"_B_repB"), paste0(PBID,"_repA"))) %>%
+                   mutate(correct_id = ifelse(cor <0.5, sampleid, correct_id))
 
+matches <- matches %>%
+                   filter(.,!sampleid %in% mixed$sampleid) %>%
+                   mutate(correct_id= sampleid) %>%
+                   rbind(mixed) %>%
+                   mutate(correct_id=gsub("_rep1","_repA", correct_id)) %>%
+                   mutate(correct_id=gsub("_rep2","_B_repB", correct_id)) %>%
+                   mutate(correct_id=ifelse(! PBID %in% duplicates$PBID, gsub("_B_repB","_repA", correct_id),correct_id)) %>%
+                   arrange(PBID)
 
-### Creating new annotation dataframe
-#   annot: 9,788 x 3
-new_annots <- annot %>%
-  select(protein_id) %>%                                             # Get only protein ID column
-  filter(protein_id %in% rownames(raw.expr)) %>%                     # Get proteins in rownames of raw.expr because there is less than 50% NAs for these protein expressions
-  filter(!(grepl('##', protein_id) | grepl('sp',protein_id)))  %>%   # Filter out proteins that start with '##' or 'sp'
-  merge(., ensembl_90_prot, by.x = 'protein_id', all.x = TRUE) %>%   # Merge protein ID with ensembl 94 annotation
-  filter(prot.chr %in% c(1:19,'X')) %>%                              # Get all proteins in 1-19, X chromosomes
-  mutate(gene_id = gsub("\\..*","", gene_id))                        # Remove .* in gene ids
-table(new_annots$prot.chr)
-
-
-
-
-#  Get gene annotations
-gene_annots <- batchGenes(as.list(unique(new_annots$gene_id)), version = 90, species = 'Mm')
-new_annots  <- merge(new_annots, gene_annots[,c('gene_id','chromosome','start','end','strand', 'name','synonyms','symbol')], by = 'gene_id', all.x = TRUE)
-stopifnot(new_annots$prot.chr == new_annots$chromosome | new_annots$gene_symbol == new_annots$synonyms)
-
-
-
-
-
-
-#  new_annots: 7,853 x 11
-new_annots <- new_annots %>%
-  mutate(start  = as.numeric(start) / 1e6,
-         end    = as.numeric(end) / 1e6,
-         middle = (start + end) * 0.5) %>%
-  dplyr::rename(chr=chromosome) %>%
-  select(protein_id, gene_id, transcript_id, symbol, chr, 
-         start, end, middle, strand, gene_biotype, description) %>%
-  arrange(protein_id)
-
-
-
-#  Find nearest marker
-nearest_marker <- character(length = nrow(new_annots))
-for(i in 1:nrow(new_annots)){
-  
-  sub <- subset(markers, chr == new_annots$chr[i])
-  nearest_marker[i] <- sub$marker[which.min(abs(sub$pos - new_annots$start[i]))]
-  
-}
-
-
-
-new_annots$nearest.marker.id <- nearest_marker 
-rownames(new_annots) <- new_annots$protein_id
-
-
+#   Correcting a typo error in matches data frame
+matches$correct_id[90] <- 'PB360.70_repA'
 
 
 
@@ -96,6 +68,7 @@ rownames(new_annots) <- new_annots$protein_id
 
 
 ### Creating covar.factos
+#
 #   covar.factors: 2 x 5
 covar.factors <- data.frame(column.name = c('sex','tmt_label'),
                             display.name = c('Sex','TMT Label'),
@@ -116,45 +89,11 @@ covar.factors <- data.frame(column.name = c('sex','tmt_label'),
 
 
 
-
-### Creating new genoprobs. Not sure how to make probs, a 2d matrix, into 3d.
-#       Lines 120-144 is taken from Selcan (Munger Lab).
+### Changing genotype sample IDs to correct labels
 #
-#   probs:      552,040 x 195
-#   genoprobs:  195 x 8 x *
-matches <- matches %>%
-  filter(!is.na(sampleid.esc_prot)) %>%
-  select(-ends_with(".npc_rna"), -ends_with(".esc_rna")) %>%
-  distinct() %>%
-  arrange(PBID)
-colnames(matches) <- gsub("\\.esc_prot","",colnames(matches))
-duplicates        <- matches[duplicated(matches$top_muga),]
-
-mixed   <- matches %>%
-  filter(mixup ==T & !endsWith(matches$sampleid,"_rep2") ) %>%
-  mutate(correct_id = ifelse(PBID %in% duplicates$PBID, paste0(PBID,"_B_repB"), paste0(PBID,"_repA"))) %>%
-  mutate(correct_id = ifelse(cor <0.5, sampleid, correct_id))
-
-matches <- matches %>%
-  filter(.,!sampleid %in% mixed$sampleid) %>%
-  mutate(correct_id= sampleid) %>%
-  rbind(mixed) %>%
-  mutate(correct_id=gsub("_rep1","_repA", correct_id)) %>%
-  mutate(correct_id=gsub("_rep2","_B_repB", correct_id)) %>%
-  mutate(correct_id=ifelse(! PBID %in% duplicates$PBID, gsub("_B_repB","_repA", correct_id),correct_id)) %>%
-  arrange(PBID)
-
-#   Correcting a typo error in matches data frame
-matches$correct_id[90] <- 'PB360.70_repA'
-
-
-
-
-
-
 #   Making sure genoprobs sample id are consistent for every chromosome
 for(i in 2:length(genoprobs)){
-  stopifnot(dimnames(genoprobs[[i]])[[1]] == dimnames(genoprobs[[1]])[[1]])
+    stopifnot(dimnames(genoprobs[[i]])[[1]] == dimnames(genoprobs[[1]])[[1]])
 }
 
 
@@ -164,7 +103,7 @@ gn <- dimnames(genoprobs[[1]])[[1]]
 cc <- match(dimnames(genoprobs[[1]])[[1]], matches$sampleid)
 
 for(i in 1:length(genoprobs)){
-  dimnames(genoprobs[[i]])[[1]] <- matches$correct_id[cc]
+    dimnames(genoprobs[[i]])[[1]] <- matches$correct_id[cc]
 }
 
 
@@ -208,34 +147,30 @@ col_name  <- gsub('.' ,'~', colnames(new_raw), fixed = TRUE)
 col_name  <- gsub('X' ,'', col_name, fixed = TRUE)
 colnames(new_raw) <- col_name
 new_raw <- new_raw[,colnames(new_raw) %in% orig_samples_key$colname]
-new_raw <- new_raw[rownames(new_raw) %in% new_annots$protein_id, ]
+
 
 
 #   Fixing column names
 oc <- match(colnames(new_raw), orig_samples_key$colname)
 colnames(new_raw) <- orig_samples_key$full_id[oc]
-cn <- colnames(new_raw)
 cc <- match(colnames(new_raw), matches$sampleid)
 colnames(new_raw) <-  matches$correct_id[cc]
-new_raw <- as.matrix(new_raw)
+
+
+
+#   Make 0 NAs, remove samples, and remove proteins with more than 50% NAs
+#   Raw: 7,949 x 191
 new_raw[new_raw == 0] <- NA
 new_raw <- new_raw[order(rownames(new_raw)), order(colnames(new_raw))]
-
-
-
-
-
-
-
-#   Remove these samples
-#   Raw: 7,853 x 191
 new_raw <- new_raw %>%
                    as.data.frame() %>%
                    select(-PB360.45_B_repB,
                           -PB360.76_B_repB,
                           -PB366.18_B_repB,
                           -PB360.93_B_repB) %>%
-                  as.matrix()
+                   as.matrix()
+rm_prots <- which(rowSums(is.na(new_raw)) > ncol(new_raw) * .5 )
+new_raw  <- new_raw[-rm_prots,]
 
 
 
@@ -246,9 +181,85 @@ new_raw <- new_raw %>%
 
 
 
-### Create new quantile normalized proteins
+
+
+
+### Create new annots
+# 
+#   annot    : 9,788 x 3
+new_annots <- annot %>%
+                    select(protein_id) %>%                                             # Get only protein ID column
+                    filter(protein_id %in% rownames(new_raw)) %>%                      # Get proteins in rownames of raw.expr because there is less than 50% NAs for these protein expressions
+                    filter(!(grepl('##', protein_id) | grepl('sp',protein_id)))  %>%   # Filter out proteins that start with '##' or 'sp'
+                    merge(., ensembl_90_prot, by.x = 'protein_id', all.x = TRUE) %>%   # Merge protein ID with ensembl 94 annotation
+                    filter(prot.chr %in% c(1:19,'X')) %>%                              # Get all proteins in 1-19, X chromosomes
+                    mutate(gene_id = gsub("\\..*","", gene_id))                        # Remove .* in gene ids
+table(new_annots$prot.chr)
+
+
+
+#  Get gene annotations
+gene_annots <- batchGenes(as.list(unique(new_annots$gene_id)), version = 90, species = 'Mm')
+new_annots  <- merge(new_annots, gene_annots[,c('gene_id','chromosome','start','end','strand', 'name','synonyms','symbol')], by = 'gene_id', all.x = TRUE)
+stopifnot(new_annots$prot.chr == new_annots$chromosome | new_annots$gene_symbol == new_annots$synonyms)
+
+
+
+#  new_annots: 7,858 x 11
+new_annots <- new_annots %>%
+                         mutate(start  = as.numeric(start) / 1e6,
+                                end    = as.numeric(end) / 1e6,
+                                middle = (start + end) * 0.5) %>%
+                         dplyr::rename(chr=chromosome) %>%
+                         select(protein_id, gene_id, transcript_id, symbol, chr, 
+                                start, end, middle, strand, gene_biotype, description) %>%
+                         arrange(protein_id)
+
+
+
+#  Find nearest marker
+#
+#  new_annots: 7,858 x 12
+nearest_marker <- character(length = nrow(new_annots))
+for(i in 1:nrow(new_annots)){
+  
+    sub <- subset(markers, chr == new_annots$chr[i])
+    nearest_marker[i] <- sub$marker[which.min(abs(sub$pos - new_annots$start[i]))]
+  
+}
+new_annots$nearest.marker.id <- nearest_marker 
+rownames(new_annots) <- new_annots$protein_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Filter raw to have  proteins from 1-19, X chromosomes
+#   new_raw:  7,949 x 195
+#   new_norm: 7,858 x 191
+new_raw <- new_raw[rownames(new_raw) %in% new_annots$protein_id,]
+
+
+
+
+
+
+
+
+
+### Create new norm dataset by log transformation
 #   expr.qnorm: 7,945 x 195
-#   new_norm:   7,853 x 191
+#   new_norm:   7,858 x 191
 new_norm <- log(new_raw)
 dimnames(new_norm) <- dimnames(new_raw)
 
@@ -259,12 +270,9 @@ dimnames(new_norm) <- dimnames(new_raw)
 
 
 
-
-
-
 ### Creating new rankz proteins
 #   expr:      195 x 7,945
-#   new_rankz: 191 x 7,853
+#   new_rankz: 191 x 7,858
 rankZ <- function (x) {
   x <- rank(x, na.last = "keep", ties.method = "average")/(sum(!is.na(x)) + 1)
   qnorm(x)
@@ -302,13 +310,14 @@ new_samples <- orig_samples_key %>%
 rownames(new_samples) <- new_samples$mouse.id
 
 
-
-
-
-
 #  Wrong sex labels
 new_samples['PB359.70_repA', 'sex']   <- 'F'
 new_samples['PB361.72_repA', 'sex']   <- 'F'
+
+
+
+
+
 
 
 
