9011dadcd3dfed541585c88f7ff61e6254aadc1f
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-02-15 10:24:00 -0500

    Update function_lm_miQTL.R

--- Sample_Mixups/function_lm_miQTL.R
+++ Sample_Mixups/function_lm_miQTL.R
@@ -1,6 +1,6 @@
 ### Helper lm function. Runs lm on all pQTL that pass threshold
 #    1.) threshold to subset the lod.peaks data
-lm_MUGA_miQTL <- 
+lm_miQTL <- 
   function(threshold){
     
     ### Make threshold parameter a vector to loop over
