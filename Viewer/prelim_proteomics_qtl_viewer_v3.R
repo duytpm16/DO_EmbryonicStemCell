0c62b5b284d3203cea598bb2b411f810a8bc4e2b
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-02-19 12:11:47 -0500

    Update prelim_proteomics_qtl_viewer_v3.R

--- Viewer/prelim_proteomics_qtl_viewer_v3.R
+++ Viewer/prelim_proteomics_qtl_viewer_v3.R
@@ -33,26 +33,26 @@ orig_samples_key <- readRDS('~/Desktop/Munger Embrynic Stem Cells/Proteomics/Ori
 ### Get correct ID labels for samples.
 #   Lines 35-55 is taken from Selcan (Munger Lab).
 matches <- matches %>%
-  filter(!is.na(sampleid.esc_prot)) %>%
-  select(-ends_with(".npc_rna"), -ends_with(".esc_rna")) %>%
-  distinct() %>%
-  arrange(PBID)
+                   filter(!is.na(sampleid.esc_prot)) %>%
+                   select(-ends_with(".npc_rna"), -ends_with(".esc_rna")) %>%
+                   distinct() %>%
+                   arrange(PBID)
 colnames(matches) <- gsub("\\.esc_prot","",colnames(matches))
 duplicates        <- matches[duplicated(matches$top_muga),]
 
 mixed   <- matches %>%
-  filter(mixup ==T & !endsWith(matches$sampleid,"_rep2") ) %>%
-  mutate(correct_id = ifelse(PBID %in% duplicates$PBID, paste0(PBID,"_B_repB"), paste0(PBID,"_repA"))) %>%
-  mutate(correct_id = ifelse(cor <0.5, sampleid, correct_id))
+                   filter(mixup ==T & !endsWith(matches$sampleid,"_rep2") ) %>%
+                   mutate(correct_id = ifelse(PBID %in% duplicates$PBID, paste0(PBID,"_B_repB"), paste0(PBID,"_repA"))) %>%
+                   mutate(correct_id = ifelse(cor <0.5, sampleid, correct_id))
 
 matches <- matches %>%
-  filter(.,!sampleid %in% mixed$sampleid) %>%
-  mutate(correct_id= sampleid) %>%
-  rbind(mixed) %>%
-  mutate(correct_id=gsub("_rep1","_repA", correct_id)) %>%
-  mutate(correct_id=gsub("_rep2","_B_repB", correct_id)) %>%
-  mutate(correct_id=ifelse(! PBID %in% duplicates$PBID, gsub("_B_repB","_repA", correct_id),correct_id)) %>%
-  arrange(PBID)
+                   filter(.,!sampleid %in% mixed$sampleid) %>%
+                   mutate(correct_id= sampleid) %>%
+                   rbind(mixed) %>%
+                   mutate(correct_id=gsub("_rep1","_repA", correct_id)) %>%
+                   mutate(correct_id=gsub("_rep2","_B_repB", correct_id)) %>%
+                   mutate(correct_id=ifelse(! PBID %in% duplicates$PBID, gsub("_B_repB","_repA", correct_id),correct_id)) %>%
+                   arrange(PBID)
 
 #   Correcting a typo error in matches data frame
 matches$correct_id[90] <- 'PB360.70_repA'
@@ -93,7 +93,7 @@ covar.factors <- data.frame(column.name = c('sex','tmt_label'),
 #
 #   Making sure genoprobs sample id are consistent for every chromosome
 for(i in 2:length(genoprobs)){
-  stopifnot(dimnames(genoprobs[[i]])[[1]] == dimnames(genoprobs[[1]])[[1]])
+    stopifnot(dimnames(genoprobs[[i]])[[1]] == dimnames(genoprobs[[1]])[[1]])
 }
 
 
@@ -103,7 +103,7 @@ gn <- dimnames(genoprobs[[1]])[[1]]
 cc <- match(dimnames(genoprobs[[1]])[[1]], matches$sampleid)
 
 for(i in 1:length(genoprobs)){
-  dimnames(genoprobs[[i]])[[1]] <- matches$correct_id[cc]
+    dimnames(genoprobs[[i]])[[1]] <- matches$correct_id[cc]
 }
 
 
@@ -163,12 +163,12 @@ colnames(new_raw) <-  matches$correct_id[cc]
 new_raw[new_raw == 0] <- NA
 new_raw <- new_raw[order(rownames(new_raw)), order(colnames(new_raw))]
 new_raw <- new_raw %>%
-  as.data.frame() %>%
-  select(-PB360.45_B_repB,
-         -PB360.76_B_repB,
-         -PB366.18_B_repB,
-         -PB360.93_B_repB) %>%
-  as.matrix()
+                   as.data.frame() %>%
+                   select(-PB360.45_B_repB,
+                          -PB360.76_B_repB,
+                          -PB366.18_B_repB,
+                          -PB360.93_B_repB) %>%
+                   as.matrix()
 rm_prots <- which(rowSums(is.na(new_raw)) > ncol(new_raw) * .5 )
 new_raw  <- new_raw[-rm_prots,]
 
@@ -188,12 +188,12 @@ new_raw  <- new_raw[-rm_prots,]
 # 
 #   annot    : 9,788 x 3
 new_annots <- annot %>%
-  select(protein_id) %>%                                             # Get only protein ID column
-  filter(protein_id %in% rownames(new_raw)) %>%                      # Get proteins in rownames of raw.expr because there is less than 50% NAs for these protein expressions
-  filter(!(grepl('##', protein_id) | grepl('sp',protein_id)))  %>%   # Filter out proteins that start with '##' or 'sp'
-  merge(., ensembl_90_prot, by.x = 'protein_id', all.x = TRUE) %>%   # Merge protein ID with ensembl 94 annotation
-  filter(prot.chr %in% c(1:19,'X')) %>%                              # Get all proteins in 1-19, X chromosomes
-  mutate(gene_id = gsub("\\..*","", gene_id))                        # Remove .* in gene ids
+                    select(protein_id) %>%                                             # Get only protein ID column
+                    filter(protein_id %in% rownames(new_raw)) %>%                      # Get proteins in rownames of raw.expr because there is less than 50% NAs for these protein expressions
+                    filter(!(grepl('##', protein_id) | grepl('sp',protein_id)))  %>%   # Filter out proteins that start with '##' or 'sp'
+                    merge(., ensembl_90_prot, by.x = 'protein_id', all.x = TRUE) %>%   # Merge protein ID with ensembl 94 annotation
+                    filter(prot.chr %in% c(1:19,'X')) %>%                              # Get all proteins in 1-19, X chromosomes
+                    mutate(gene_id = gsub("\\..*","", gene_id))                        # Remove .* in gene ids
 table(new_annots$prot.chr)
 
 
@@ -207,13 +207,13 @@ stopifnot(new_annots$prot.chr == new_annots$chromosome | new_annots$gene_symbol 
 
 #  new_annots: 7,858 x 11
 new_annots <- new_annots %>%
-  mutate(start  = as.numeric(start) / 1e6,
-         end    = as.numeric(end) / 1e6,
-         middle = (start + end) * 0.5) %>%
-  dplyr::rename(chr=chromosome) %>%
-  select(protein_id, gene_id, transcript_id, symbol, chr, 
-         start, end, middle, strand, gene_biotype, description) %>%
-  arrange(protein_id)
+                         mutate(start  = as.numeric(start) / 1e6,
+                                end    = as.numeric(end) / 1e6,
+                                middle = (start + end) * 0.5) %>%
+                         dplyr::rename(chr=chromosome) %>%
+                         select(protein_id, gene_id, transcript_id, symbol, chr, 
+                                start, end, middle, strand, gene_biotype, description) %>%
+                         arrange(protein_id)
 
 
 
@@ -223,8 +223,8 @@ new_annots <- new_annots %>%
 nearest_marker <- character(length = nrow(new_annots))
 for(i in 1:nrow(new_annots)){
   
-  sub <- subset(markers, chr == new_annots$chr[i])
-  nearest_marker[i] <- sub$marker[which.min(abs(sub$pos - new_annots$start[i]))]
+    sub <- subset(markers, chr == new_annots$chr[i])
+    nearest_marker[i] <- sub$marker[which.min(abs(sub$pos - new_annots$start[i]))]
   
 }
 new_annots$nearest.marker.id <- nearest_marker 
@@ -301,13 +301,13 @@ orig_samples_key$id       <- matches$PBID[id.index]
 
 
 new_samples <- orig_samples_key %>%
-  dplyr::rename(mouse.id=full_id,
-                PBID=id,
-                MUGA.id=array_id,
-                orig.name=colname) %>%
-  select(mouse.id,PBID,MUGA.id, sex, tmt_label,tmt_num,orig.name) %>%
-  arrange(mouse.id) %>%
-  filter(!mouse.id %in% c('PB360.45_B_repB','PB360.76_B_repB','PB366.18_B_repB','PB360.93_B_repB'))
+                                dplyr::rename(mouse.id=full_id,
+                                              PBID=id,
+                                              MUGA.id=array_id,
+                                              orig.name=colname) %>%
+                                select(mouse.id,PBID,MUGA.id, sex, tmt_label,tmt_num,orig.name) %>%
+                                arrange(mouse.id) %>%
+                                filter(!mouse.id %in% c('PB360.45_B_repB','PB360.76_B_repB','PB366.18_B_repB','PB360.93_B_repB'))
 rownames(new_samples) <- new_samples$mouse.id
 
 
